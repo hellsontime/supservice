f15e58060e058f103294bb4106702d56bccf46d7
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-14 10:57:05 +0300

    ticket repository implementation + interface

---
+++ src/Repository/TicketRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Ticket;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Ticket|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Ticket|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Ticket[]    findAll()
+ * @method Ticket[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TicketRepository extends ServiceEntityRepository implements TicketRepositoryInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Ticket::class);
+    }
+
+    public function getUserTickets(int $userId): array
+    {
+        return $this->findBy(['userId' => $userId]);
+    }
+
+    public function createTicket(array $requestBody)
+    {
+        // TODO: Implement createTicket() method.
+    }
+}
