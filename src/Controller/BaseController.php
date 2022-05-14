08c1d1d1d6072baed7761ed0b4267881f5af314c
Artem Davidyan <artem.davidyan@innowise-group.com>
2022-05-14 10:56:33 +0300

    Base controller implementation + methods

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class BaseController extends AbstractController
+{
+    protected function transformJsonBody(Request $request): Request
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if ($data === null) {
+            return $request;
+        }
+
+        $request->request->replace($data);
+
+        return $request;
+    }
+
+    protected function getRequestBody(Request $request): array
+    {
+        return json_decode($request->getContent(), true);
+
+    }
+}
