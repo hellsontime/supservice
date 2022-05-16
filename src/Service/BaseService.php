e5b115cbd9e3669d93c3a2414975560cc598fa2d
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-16 12:40:01 +0300

    BaseService class implementation

---
+++ src/Service/BaseService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class BaseService implements BaseServiceInterface
+{
+    /**
+     * Returns a JSON response
+     *
+     * @param array $data
+     * @param int $status
+     * @param array $headers
+     * @return JsonResponse
+     */
+    public function response(array $data, int $status = 200, array $headers = []): JsonResponse
+    {
+        return new JsonResponse($data, $status, $headers);
+    }
+}
