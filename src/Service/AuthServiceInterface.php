59591cd07d5734a7addb9fa5158f9a5bb642609a
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-19 10:04:12 +0300

    AuthService implementation + interface

---
+++ src/Service/AuthServiceInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+interface AuthServiceInterface
+{
+    public function register(array $requestBody);
+//    public function login(LoginAuthRequest $fields);
+//    public function logout(Request $request);
+}
+
