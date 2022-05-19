1fa31807476d060a4573bdf69ff07271df18185c
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-19 13:32:38 +0300

    TicketRepository updateUserTicket and deleteUserTicket methods implementation + interface

--- src/Repository/TicketRepositoryInterface.php
+++ src/Repository/TicketRepositoryInterface.php
@@ -2,9 +2,13 @@
 
 namespace App\Repository;
 
+use App\Entity\Ticket;
+
 interface TicketRepositoryInterface
 {
     public function getUserTickets(int $userId);
     public function createTicket(array $requestBody);
     public function getUserTicketById(int $ticketId);
+    public function updateUserTicket(array $requestBody, Ticket $ticket);
+    public function deleteUserTicket(Ticket $ticket);
 }
