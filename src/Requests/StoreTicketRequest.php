46a752fdc83d5e8a7fff488545e65934d96d2db0
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-19 10:03:02 +0300

    StoreTicketRequest add Length constraint

--- src/Requests/StoreTicketRequest.php
+++ src/Requests/StoreTicketRequest.php
@@ -4,11 +4,13 @@ namespace App\Requests;
 
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Constraints\Length;
 
 class StoreTicketRequest extends BaseRequest
 {
     #[Type('string')]
     #[NotBlank()]
+    #[Length(null, null, 140)]
     protected string $title;
 
     #[Type('string')]
