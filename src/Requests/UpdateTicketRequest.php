431ea3f140a9d71ab9197e17d7c3f019315f675e
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-19 13:29:59 +0300

    UpdateTicketRequest Implementation

---
+++ src/Requests/UpdateTicketRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Requests;
+
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Constraints\Length;
+
+class UpdateTicketRequest extends BaseRequest
+{
+    #[Type('string')]
+    #[NotBlank()]
+    #[Length(null, null, 140)]
+    protected string $title;
+
+    #[Type('string')]
+    #[NotBlank()]
+    protected string $description;
+}
