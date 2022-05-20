63b5aeac0ad31a8ecb250108a932fff0ad8d483a
Artem Davidyan <artem.davidyan@innowise-group.com>
2022-05-20 18:16:06 +0300

    Migrations recreate before entities refactor

--- migrations/Version20220520132019.php
+++ migrations/Version20220520132019.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220517081618 extends AbstractMigration
+final class Version20220520132019 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220517081618 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE message (message_id INT AUTO_INCREMENT NOT NULL, ticket_id INT NOT NULL, sender_user_id INT NOT NULL, content LONGTEXT NOT NULL, PRIMARY KEY(message_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE message');
+
     }
 }
--- migrations/Version20220520132440.php
+++ migrations/Version20220520132440.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220517120316 extends AbstractMigration
+final class Version20220520132440 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20220517120316 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (user_id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F700047D2 FOREIGN KEY (ticket_id) REFERENCES ticket (id)');
+        $this->addSql('CREATE INDEX IDX_B6BD307F700047D2 ON message (ticket_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F700047D2');
+        $this->addSql('DROP INDEX IDX_B6BD307F700047D2 ON message');
     }
 }
