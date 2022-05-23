c0234b378beb181fc4655149c422d0bbc5e6be86
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-23 12:56:15 +0300

    EnsureTicketBelongsToUser Event implementation

---
+++ src/Event/EnsureTicketBelongsToUser.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Event;
+
+use App\Repository\TicketRepositoryInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Security;
+
+class EnsureTicketBelongsToUser implements EventSubscriberInterface
+{
+    private Security $_security;
+    private TicketRepositoryInterface $_ticketRepository;
+
+    public function __construct(Security $security, TicketRepositoryInterface $ticketRepository)
+    {
+        $this->_security = $security;
+        $this->_ticketRepository = $ticketRepository;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            \Symfony\Component\HttpKernel\KernelEvents::REQUEST => 'onKernelRequest',
+        ];
+    }
+
+    public function onKernelRequest($event)
+    {
+        if (!$event->isMasterRequest()) { // Обрабатываем только основной request
+            return;                       // Игнорируем forward и render controller в твиге
+        }
+
+        $handleRoutes = [
+            'ticket-show',
+            'ticket-update',
+            'ticket-destroy',
+            'message-index',
+            'message-store',
+            'message-show',
+            'message-update',
+            'message-destroy',
+        ];
+
+        $request = $event->getRequest();
+        $routeName = $request->attributes->get('_route');
+
+        if (in_array($routeName, $handleRoutes)) {
+            $ticketId = $request->get('ticketId');
+            $userId = $this->_security->getUser()->getId();
+
+            $ticket = $this->_ticketRepository->findOneBy([
+                'id' => $ticketId,
+                'user_id' => $userId,
+            ]);
+
+            if (!$ticket) {
+                $event->setResponse(new JsonResponse(['message' => 'Ticket not found'], 404));
+            }
+        }
+    }
+}
