49011fca06cdfd863790670e5bdc7fd05fa9353a
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-07 14:14:44 +0300

    TicketRepositoryInterface methods added

--- src/Repository/TicketRepositoryInterface.php
+++ src/Repository/TicketRepositoryInterface.php
@@ -7,8 +7,10 @@ use App\Entity\Ticket;
 interface TicketRepositoryInterface
 {
     public function getUserTickets(int $userId);
+    public function getSupportTickets(int $supportId);
     public function createTicket(array $requestBody);
     public function getUserTicketById(int $ticketId);
     public function updateUserTicket(array $requestBody, Ticket $ticket);
     public function deleteUserTicket(Ticket $ticket);
+    public function countSupportOpenedTickets(int $supportId);
 }
