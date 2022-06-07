89f19f8ba775f4c2a8eb03c981c45bbcc17d2ea3
Artem Davidyan <artem.davidyan@innowise-group.com>
2022-06-07 18:13:45 +0300

    EnsureTicketBelongsToSupport event implementation

---
+++ src/Event/EnsureTicketBelongsToSupport.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Event;
+
+use App\Repository\TicketRepositoryInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Security\Core\Security;
+
+class EnsureTicketBelongsToSupport implements EventSubscriberInterface
+{
+    private Security $_security;
+    private TicketRepositoryInterface $_ticketRepository;
+
+    public function __construct(Security $security, TicketRepositoryInterface $ticketRepository)
+    {
+        $this->_security = $security;
+        $this->_ticketRepository = $ticketRepository;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            \Symfony\Component\HttpKernel\KernelEvents::REQUEST => ['onKernelRequest', -5],
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $handleRoutes = [
+            'support-ticket-show',
+            'support-status-update',
+        ];
+
+        $request = $event->getRequest();
+        $routeName = $request->attributes->get('_route');
+
+        if (in_array($routeName, $handleRoutes)) {
+            $ticketId = $request->get('ticketId');
+            $userId = $this->_security->getUser()->getId();
+
+            $ticket = $this->_ticketRepository->findOneBy([
+                'id' => $ticketId,
+                'support_id' => $userId,
+            ]);
+
+            if (!$ticket) {
+                $event->setResponse(new JsonResponse(['message' => 'Ticket not found'], 404));
+            }
+        }
+    }
+}
