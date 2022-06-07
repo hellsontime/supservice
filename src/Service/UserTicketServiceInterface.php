ddb9a180f260fb31a45ef52c82bc7ec68146ead7
Artem Davidyan <artem.davidyan@innowise-group.com>
2022-06-07 18:18:29 +0300

    UserTicketService implementation + interface

--- src/Service/UserTicketServiceInterface.php
+++ src/Service/UserTicketServiceInterface.php
@@ -2,12 +2,11 @@
 
 namespace App\Service;
 
-interface TicketServiceInterface
+interface UserTicketServiceInterface
 {
     public function getUserTickets(int $userId);
-    public function getSupportTickets(int $supportId);
     public function createTicket(array $requestBody);
-    public function getUserTicketById(int $userId, int $ticketId);
+    public function getUserTicketById(int $ticketId);
     public function updateUserTicket(array $requestBody, int $ticketId);
     public function deleteUserTicket(int $userId, int $ticketId);
 }
