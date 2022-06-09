55567c21d2a68206dd9432d039b7301004ecda48
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-09 14:20:20 +0300

    BaseFixture refactor - dynamic variables added

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -7,8 +7,27 @@ use Doctrine\Persistence\ObjectManager;
 
 abstract class BaseFixture extends Fixture
 {
+    private array|false $config;
+
+    protected int $number_of_supports;
+    protected int $number_of_users;
+    protected int $number_of_tickets_per_user;
+    protected int $number_of_tickets_per_support;
+    protected int $number_of_messages_per_ticket;
+
     /** @var ObjectManager */
     private ObjectManager $manager;
+
+    public function __construct()
+    {
+        $this->config = parse_ini_file('config.ini');
+
+        $this->number_of_supports = $this->config['number_of_supports'];
+        $this->number_of_users = $this->config['number_of_users'];
+        $this->number_of_tickets_per_user = $this->config['number_of_tickets_per_user'];
+        $this->number_of_messages_per_ticket = $this->config['number_of_messages_per_ticket'];
+        $this->number_of_tickets_per_support = intdiv($this->number_of_users * $this->number_of_tickets_per_user, $this->number_of_supports);
+    }
 
     abstract protected function loadData(ObjectManager $manager);
 
