f2f35433f82df5efad14abacef5a9fac54699034
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-09 14:21:32 +0300

    TicketFixtures refactor to use config constants

--- src/DataFixtures/F1_TicketFixtures.php
+++ src/DataFixtures/F1_TicketFixtures.php
@@ -14,20 +14,28 @@ class F1_TicketFixtures extends BaseFixture
 
     public function __construct()
     {
+        parent::__construct();
+
         $this->faker = Factory::create();
     }
 
     public function loadData(ObjectManager $manager)
     {
-        $fistUserId = $this->getReference(User::class.'_5')->getId();
+        $fistUserId = $this->getReference(User::class.'_'.$this->number_of_supports)->getId();
 
-        $this->createMany(Ticket::class, 25, function (Ticket $ticket, int $count) use ($fistUserId)
+        $this->createMany(Ticket::class, $this->number_of_users * $this->number_of_tickets_per_user, function (Ticket $ticket, int $count) use ($fistUserId)
         {
             $ticket->setTitle($this->faker->sentence(3));
             $ticket->setDescription($this->faker->sentence(9));
 
-            $ticket->setUserId($fistUserId + $count);
-            $ticket->setSupportId($this->getReference(User::class.'_'.intdiv($count, 5))->getId());
+            $ticket->setUserId(
+                intdiv($fistUserId * $this->number_of_tickets_per_user + $count, $this->number_of_tickets_per_user)
+            );
+            $ticket->setSupportId(
+                $this->getReference(
+                    User::class.'_'.intdiv($count, $this->number_of_tickets_per_support)
+                )->getId()
+            );
             $ticket->setStatus(['opened', 'closed', 'frozen'][rand(0,2)]);
         });
 
